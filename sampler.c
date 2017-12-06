1e4d53a1f08013ee696c344a233c591bddd10740
inselc <13700483+inselc@users.noreply.github.com>
2017-12-06 10:12:35 +0100

    Refactor sampler
    Sampler module will now only handle periodic ADC triggering

--- sampler.c
+++ sampler.c
@@ -19,17 +19,15 @@
 
 /*- Macros ------------------------------------------------------------------*/
 #define IS_SAMPLER_TIMEBASE(x)  ((x)>0x00 && (x)<EN_SAMPLER_TIMEBASE_MAX)
-#define IS_SAMPLER_TRIGSRC(x)   (((x) == EN_SAMPLER_TRIGSRC_DISABLE)    ||    \
-                                 ((x) == EN_SAMPLER_TRIGSRC_CONTINUOUS) ||    \
-                                 ((x) == EN_SAMPLER_TRIGSRC_COMPARATOR) ||    \
-                                 ((x) == EN_SAMPLER_TRIGSRC_EXTERNAL))
-#define IS_SAMPLER_TRIGMODE(x)  (((x) == EN_SAMPLER_TRIGMODE_STOP)     ||     \
-                                 ((x) == EN_SAMPLER_TRIGMODE_SINGLE)   ||     \
-                                 ((x) == EN_SAMPLER_TRIGMODE_NORMAL))
 
 
 /*- Global variables --------------------------------------------------------*/
 global int16_t g_aiSampleBuffer[SAMPLER_BUF_LEN] = {[0 ... SAMPLER_BUF_LEN-1] = SAMPLER_SAMPLE_INVALID};
+global volatile bool g_bSamplerDataReady;   /* Used by ISR                   */
+
+
+/*- Module variables --------------------------------------------------------*/
+static volatile bool bSamplerSlowTimebase;
 
 
 /*- Inline functions --------------------------------------------------------*/
@@ -41,126 +39,9 @@ static inline void vSamplerClearBuffer(int16_t* p_aiBuffer)
     memset(p_aiBuffer, SAMPLER_SAMPLE_INVALID, SAMPLER_BUF_LEN * sizeof(*p_aiBuffer));
 }
 
-
-/*- Local prototypes --------------------------------------------------------*/
-static void vSamplerStop(void);
-static void vSamplerStartTimer(void);
-
-
-/**
- *  @brief  Initialise the sampler module
- *
- *  Configures GPT1 as potential ADC trigger, and GPIO pin PE
- *                                                                           */
-void vSamplerInit(void)
+static inline void vSamplerSetupDMA(void)
 {
-    /* Setup GPIO Port K Run Mode Clock                                      */
-    SysCtlPeripheralEnable(SAMPLER_EXT_RCGC);
-    /* Setup GPTM Timer 1 Run Mode Clock                                     */
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
-    asm("\tnop;\r\n\tnop;\r\n\tnop;\r\n");
-
-    /* Prepare PK7 as trigger input                                          */
-    /* not yet implemented                                                   */
-
-    /* Prepare GPT1 as ADC trigger                                           */
-    TimerClockSourceSet(TIMER1_BASE, TIMER_CLOCK_SYSTEM);
-    TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
-    TimerPrescaleSet(TIMER1_BASE, TIMER_BOTH, 0);
-    TimerMatchSet(TIMER1_BASE, TIMER_BOTH, 0);
-    TimerLoadSet(TIMER1_BASE, TIMER_BOTH, EN_SAMPLER_TIMEBASE_1ms);
-    TimerControlTrigger(TIMER1_BASE, TIMER_BOTH, true);
-
-    /* Configure ADC Sample 0 (Vin) complete interrupt                       */
-    IntEnable(INT_ADC0SS0);
-
-    /* Configure uDMA channel 14 for ADC0SS0                                 */
-    ADCSequenceDMAEnable(ADC0_BASE, 0);
-    uDMAChannelAttributeDisable(UDMA_CHANNEL_ADC0, UDMA_ATTR_ALL);
-    uDMAChannelControlSet(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT,
-                          UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
-}
-
-/**
- *  @brief  Reconfigure Timer ILR value for selected timebase
- *
- *  @param[in]  eTimebase   Selected timebase
- *                                                                           */
-void vSetSamplerTimebase(teSamplerTimebase eTimebase)
-{
-    assert(IS_SAMPLER_TIMEBASE(eTimebase));
-
-    TimerLoadSet(TIMER1_BASE, TIMER_BOTH, (uint32_t)eTimebase);
-}
-
-/**
- *  @brief  Configure Sampler trigger setup
- *
- *  @param[in]  eTriggerSource  Trigger source
- *  @param[in]  eTriggerMode    Trigger mode
- *                                                                           */
-void vSetSamplerTrigger(teSamplerTrigSrc eTriggerSource, teSamplerTrigMode eTriggerMode)
-{
-    assert(IS_SAMPLER_TRIGSRC(eTriggerSource));
-    assert(IS_SAMPLER_TRIGMODE(eTriggerMode));
-
-    switch (eTriggerSource)
-    {
-    case EN_SAMPLER_TRIGSRC_CONTINUOUS:
-        switch (eTriggerMode)
-        {
-        case EN_SAMPLER_TRIGMODE_NORMAL:
-        case EN_SAMPLER_TRIGMODE_SINGLE:
-            vSamplerStartTimer();
-            break;
-        default:
-            vSamplerStop();
-        }
-        break;
-    case EN_SAMPLER_TRIGSRC_COMPARATOR:
-        /* Not yet implemented                                               */
-        assert(false);
-    case EN_SAMPLER_TRIGSRC_EXTERNAL:
-        /* Not yet implemented                                               */
-        assert(false);
-    case EN_SAMPLER_TRIGSRC_DISABLE:
-    default:
-        vSamplerStop();
-    }
-}
-
-/**
- *  @brief  Stop sample timer and reconfigure ADC sequencer for SSn-bit trig.
- *                                                                           */
-static void vSamplerStop(void)
-{
-    /* Disable ADC Sequence Complete Interrupt                               */
-    //ADCIntDisable(ADC0_BASE, ADC_SEQ_VIN);
-    ADCIntDisableEx(ADC0_BASE, ADC_INT_DMA_SS0);
-    /* Reset ADC trigger to SS0-bit in PSSI register                         */
-    ADCSequenceDisable(ADC0_BASE, ADC_SEQ_VIN);
-    ADCSequenceConfigure(ADC0_BASE, ADC_SEQ_VIN, ADC_TRIGGER_PROCESSOR, 0);
-    ADCSequenceEnable(ADC0_BASE, ADC_SEQ_VIN);
-    /* Disable running timer                                                 */
-    TimerDisable(TIMER1_BASE, TIMER_BOTH);
-}
-
-/**
- *  @brief  Configure ADC sequencer to trigger from Timer 1
- *                                                                           */
-static void vSamplerStartTimer(void)
-{
-    /* Clear Sample bufffer                                                  */
-    vSamplerClearBuffer(g_aiSampleBuffer);
-    /* Enable timer                                                          */
-    TimerEnable(TIMER1_BASE, TIMER_BOTH);
-    /* Configure ADC sequencer to trigger from timer                         */
-    ADCSequenceDisable(ADC0_BASE, ADC_SEQ_VIN);
-    ADCSequenceConfigure(ADC0_BASE, ADC_SEQ_VIN, ADC_TRIGGER_TIMER, 0);
-    ADCSequenceEnable(ADC0_BASE, ADC_SEQ_VIN);
-
-    /* Enable DMA transfer to buffer                                     */
-    ADCIntEnableEx(ADC0_BASE, ADC_INT_DMA_SS0);
+    uDMAChannelDisable(UDMA_CHANNEL_ADC0);
     uDMAChannelTransferSet(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT,
                            UDMA_MODE_BASIC,
                            (void*)&ADC0_SSFIFO0_R,
@@ -169,23 +50,188 @@ static void vSamplerStartTimer(void)
     uDMAChannelEnable(UDMA_CHANNEL_ADC0);
 }
 
-/**
- *  @brief  ADC Sample Complete / DMA Xfer Complete ISR
- *                                                                           */
-void __attribute__((interrupt)) vISR_AdcVinSequencer(void)
-{
-    /* Clear interrupt flags                                                 */
-    ADCIntClearEx(ADC0_BASE, ADC_INT_SS0 | ADC_INT_DMA_SS0);
+#if 0
+/*- Local prototypes --------------------------------------------------------*/
+static void vSamplerStop(void);
+static void vSamplerStartTimer(void);
+#endif
 
-    /* To be completed                                                       */
-    ;
+/**
+ *  @brief  Initialise the sampler module
+ *
+ *  Configures GPT1 as potential ADC trigger, and GPIO pin PE
+ *                                                                           */
+void vSamplerInit(void)
+{
+    /* Setup GPTM Timer 1 Run Mode Clock                                     */
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    asm("\tnop;\r\n\tnop;\r\n\tnop;\r\n");
+
+    /* Prepare GPT1 as ADC trigger                                           */
+    TimerClockSourceSet(TIMER1_BASE, TIMER_CLOCK_SYSTEM);
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+    TimerPrescaleSet(TIMER1_BASE, TIMER_BOTH, 0);
+    TimerMatchSet(TIMER1_BASE, TIMER_BOTH, 0);
+    /* Timer load value is set by vSamplerConfigure later on.                */
+    TimerControlTrigger(TIMER1_BASE, TIMER_BOTH, true);
+
+    /* Initialise status variables                                           */
+    g_bSamplerDataReady = false;
+    bSamplerSlowTimebase = false;
+
+    /* Configure ADC sequencer to trigger from timer                         */
+    ADCSequenceDisable(ADC0_BASE, ADC_SEQ_VIN);
+    ADCSequenceConfigure(ADC0_BASE, ADC_SEQ_VIN, ADC_TRIGGER_TIMER, 0);
+    ADCSequenceEnable(ADC0_BASE, ADC_SEQ_VIN);
+
+    /* Configure uDMA channel 14 for ADC0SS0                                 */
+    ADCSequenceDMAEnable(ADC0_BASE, 0);
+    uDMAChannelAttributeDisable(UDMA_CHANNEL_ADC0, UDMA_ATTR_ALL);
+    uDMAChannelControlSet(UDMA_CHANNEL_ADC0 | UDMA_PRI_SELECT,
+                          UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
+
+    /* Configure default setup                                               */
+    vSamplerConfigure(EN_SAMPLER_TIMEBASE_10ms);
+
+    /* Enable ADC Sample Sequencer 0 Interrupt Handler                       */
+    IntEnable(INT_ADC0SS0);
 }
 
 /**
- *  @brief  Trigger interrupt called from trigger source to start sampling
- */
-void __attribute((interrupt)) vISR_SequencerTrigger(void)
+ *  @brief  Reconfigure Timer ILR value for selected timebase
+ *
+ *  @param[in]  eTimebase   Selected timebase
+ *                                                                           */
+void vSamplerConfigure(teSamplerTimebase eTimebase)
 {
-    /* Not yet implemented                                                   */
-    ;
+    assert(IS_SAMPLER_TIMEBASE(eTimebase));
+
+    /* Disable old interrupts                                                */
+    ADCIntDisableEx(ADC0_BASE, ADC_INT_SS0 | ADC_INT_DMA_SS0);
+
+    /* Stop any running timer                                                */
+    vSamplerStop();
+
+    /* Load new Timebase configuration into timer                            */
+    TimerLoadSet(TIMER1_BASE, TIMER_BOTH, (uint32_t)eTimebase);
+
+    /* Clear old buffer entries                                              */
+    vSamplerClearBuffer(g_aiSampleBuffer);
+
+    /* Force refresh on next UI cycle                                        */
+    g_bSamplerDataReady = true;
+
+    /* Determine screen update rate                                          */
+    bSamplerSlowTimebase = eTimebase >= SAMPLER_SLOW_THR;
+
+    /* Enable DMA transfer to buffer                                         */
+    vSamplerSetupDMA();
+}
+
+/**
+ *  @brief  Start sampling
+ *
+ *  Enables required interrupts and starts the Sample Timer, triggering the
+ *  ADC.
+ *                                                                           */
+void vSamplerStart(void)
+{
+#if 0
+    if (bIsSamplerBusy())
+    {
+        /* Already running                                                   */
+        return;
+    }
+#endif
+
+    /* Set update interrupt sources                                          */
+    ADCIntEnableEx(ADC0_BASE, (bSamplerSlowTimebase) ? ADC_INT_SS0 | ADC_INT_DMA_SS0 : ADC_INT_DMA_SS0);
+
+    /* Start Timer                                                           */
+    TimerEnable(TIMER1_BASE, TIMER_BOTH);
+}
+
+/**
+ *  @brief  Stop sampling
+ *
+ *  Interrupts the sampling process by disabling interrupts and stopping the
+ *  Sample Timer.
+ *  Resets the DMA transfer configuration to start at buffer index 0.
+ *                                                                           */
+void vSamplerStop(void)
+{
+    /* Disable interrupts                                                    */
+    ADCIntDisableEx(ADC0_BASE, ADC_INT_SS0 | ADC_INT_DMA_SS0);
+
+    /* Clear pending interrupts, only useful when called within ISR          */
+    IntPendClear(INT_ADC0SS0);
+
+    /* Stop timer                                                            */
+    uDMAChannelDisable(UDMA_CHANNEL_ADC0);
+    TimerDisable(TIMER1_BASE, TIMER_BOTH);
+
+    /* Reset DMA transfer                                                    */
+    vSamplerSetupDMA();
+}
+
+/**
+ *  @brief  Check ADC Sampler timer status
+ *
+ *  @return (bool)  true, if sampler is running ("busy")
+ *                                                                           */
+bool bIsSamplerBusy(void)
+{
+    return TIMER0_CTL_R & TIMER_CTL_TAEN;
+}
+
+/**
+ *  @brief  Check if sample data is ready for redrawing
+ *
+ *  @return (brief)     true, if sampler data is ready in the global buffer
+ *                                                                           */
+bool bIsSamplerDataReady(void)
+{
+    return g_bSamplerDataReady;
+}
+
+/**
+ *  @brief  Invalidate currently buffered sampler data
+ *
+ *  Used to mark sampler data as processed
+ *                                                                           */
+void vSamplerDataInvalidate(void)
+{
+    g_bSamplerDataReady = false;
+}
+
+/**
+ *  @brief  ADC Sequencer / DMA Complete interrupt
+ *
+ *  Handles stopping the sequencer after 480 samples and setting the signal
+ *  bits to allow display redrawing.
+ *
+ *  @note   Interrupt Service Routine. Do not call from linear program flow.
+ *                                                                           */
+void __attribute__((interrupt)) vISR_AdcVinSequencer(void)
+{
+    if (ADCIntStatusEx(ADC0_BASE, false) & ADC_INT_DMA_SS0)
+    {
+        /* DMA transfer complete interrupt                                   */
+
+        /* Clear interrupt flags                                             */
+        ADCIntClearEx(ADC0_BASE, ADC_INT_SS0 | ADC_INT_DMA_SS0);
+
+        /* Stop timer after last sample is saved -> "single-capture"         */
+        vSamplerStop();
+    }
+    else
+    {
+        /* ADC sequencer interrupt                                           */
+
+        /* Clear interrupt flags                                             */
+        ADCIntClearEx(ADC0_BASE, ADC_INT_SS0);
+    }
+
+    /* Signal new data ready for processing                                  */
+    g_bSamplerDataReady = true;
 }
