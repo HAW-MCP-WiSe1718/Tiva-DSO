6c3c93a9f861b74295a75e09a1393b562e1bc38f
inselc <13700483+inselc@users.noreply.github.com>
2017-12-06 10:15:14 +0100

    Rewrite main for scope application
    Rewrite the main program to use the "Scope" application

--- main.c
+++ main.c
@@ -3,43 +3,40 @@
 #include <stdint.h>                 /* Libc Standard integer                 */
 #include <stdio.h>                  /* Libc Standard I/O                     */
 #include "driverlib/interrupt.h"    /* TivaWare Interrupt Library            */
+#if 0
+#include "dma.h"                    /* DMA init functions                    */
 #include "display.h"                /* Display module                        */
 #include "touch.h"                  /* Touch Controller module               */
 #include "delay.h"                  /* Delay timer module                    */
 #include "adc.h"                    /* ADC module                            */
 #include "sampler.h"                /* Sampler module                        */
+#include "trigger.h"                /* Trigger module                        */
+#endif
 #include "system.h"                 /* System configuration module           */
+#include "ui.h"                     /* UI module: Input                      */
+#include "scope.h"
+
+
+extern void vScopeMainTouch_RunStop(tsUiCoords sTouchCoords);
 
 /**
- *  @brief  Hauptprogramm
+ *  @brief  Main program loop
  *                                                                           */
 int main(void)
 {
     /* Configure MOSC and PLL for 120MHz SysClk                              */
     vSystemClkInit();
 
-    /* Init peripherals                                                      */
-    vDelayInit();
-    vDisplayInit();
-    vTouchInit();
-    vAdcInit();
-    vSamplerInit();
+    /* Init modules                                                          */
+    vScopeInit();
 
     /* Enable interrupts                                                     */
     IntMasterEnable();
 
-    /* Clear any previous display data                                       */
-    vDisplayClear();
-
-    /* ADC Sampler Test                                                      */
-    vSetSamplerTimebase(EN_SAMPLER_TIMEBASE_1ms);
-    vSetSamplerTrigger(EN_SAMPLER_TRIGSRC_CONTINUOUS, EN_SAMPLER_TRIGMODE_NORMAL);
-
+    /* Background task                                                       */
     while(1)
     {
-        printf("CPU Temp: %3d\tADC PE1: %4d\r\n", iGetAdcChipTemperature(), g_aiSampleBuffer[0]);
-        g_aiSampleBuffer[0] = SAMPLER_SAMPLE_INVALID;
-
-        vDelay_ms(100);
+        /* Handle UI and touch input                                         */
+        vScopePoll();
     }
 }
